53d95e2a1ea52576b976529b58f455650b8a556e
lambdatestsupport <134601151+lambdatestsupport@users.noreply.github.com>
2023-11-14 12:39:55 +0530

    Update beazleyChrome11.java

--- src/main/java/beazleyChrome11.java
+++ src/main/java/beazleyChrome11.java
@@ -20,9 +20,9 @@ public class beazleyChrome11 {
 
     @BeforeMethod
     public void setUp() throws MalformedURLException {
-        String userName = "ayusha";
+        String userName = "User_Name";
 
-        String accessKey = "9iXEJQQcqL7phECZqgmR8xWEFt9KppfumY51XV5KjB21Ef0JfE";
+        String accessKey = "User_AccessKey";
 
         String gridURL = "@hub.lambdatest.com/wd/hub";
 
